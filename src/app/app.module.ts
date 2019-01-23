1ceab1a61475548dfbfb255a317eb8537171e3e5
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-01-23 21:44:04 -0200

    Import dependencioes att app.module (except FileUploadOptions due an error)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+import { Camera } from '@ionic-native/camera';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +28,12 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FileTransfer,
+    // FileUploadOptions,
+    FileTransferObject,
+    File,
+    Camera,
   ]
 })
 export class AppModule {}
