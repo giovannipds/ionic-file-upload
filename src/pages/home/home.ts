924073585fe3c58aeb11f8ab09f651c1e25f6e41
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-01-23 21:46:41 -0200

    Add method for getting image from photo library

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,4 +18,19 @@ export class HomePage {
     public loadingCtrl: LoadingController,
     public toastCtrl: ToastController) {}
 
+  getImage() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.imageURI = imageData;
+    }, (err) => {
+      console.log(err);
+      this.presentToast(err);
+    });
+  }
+
 }
